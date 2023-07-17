3de0874ef42b162e123acd4402366e9c32bed21e
AleksKar <Alekseykar92@gmail.com>
2023-07-17 22:04:58 +0100

    feat/s-1: pizzablock set usestate

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ function App() {
     <div className="App">
       <div className="wrapper">
         <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            <PizzaBlock title="Мясные" price="12"/>
-            <PizzaBlock title="Мясные" price="12"/>
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title="Мясные" price="12"/>
+              <PizzaBlock title="Мясные" price="12"/>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
